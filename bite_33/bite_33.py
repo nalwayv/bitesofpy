8b560746247f9bfb90c09021a902469c66d55d31
nalwayv <nalwayv@googlemail.com>
2019-11-16 18:30:04 +0000

    update

--- bite_33/bite_33.py
+++ bite_33/bite_33.py
@@ -54,54 +54,15 @@ def transpose(data: Any) -> List[list]:
 
 
 if __name__ == "__main__":
-    print("-" * 45)
     # DICT
     months, posts = transpose(POSTS)
     print(months)
     print(posts)
-
     print("-" * 45)
     # LIST
-    community = [
-        Member(name='Bob', 
-                since_days=145, 
-                karma_points=69,
-                bitecoin_earned=67),
-
-        Member(name='Julian',
-                since_days=157,
-                karma_points=52,
-                bitecoin_earned=14),
-
-        Member(name='Tim', 
-                since_days=9,
-                karma_points=100,
-                bitecoin_earned=10),
-
-        Member(name='Carmen',
-               since_days=155,
-               karma_points=1,
-               bitecoin_earned=49),
-
-        Member(name='Henk',
-               since_days=302,
-               karma_points=91,
-               bitecoin_earned=81),
-
-        Member(name='Sofia',
-               since_days=279,
-               karma_points=52,
-               bitecoin_earned=76),
-
-        Member(name='Bernard',
-               since_days=72,
-               karma_points=96,
-               bitecoin_earned=69)]
-
-    names, days, karma, bitecoin = transpose(community)
+    names, days, karma, bitecoin = transpose(list(_gen_community()))
     print(names)
     print(days)
     print(karma)
     print(bitecoin)
 
-    print("-" * 45)
