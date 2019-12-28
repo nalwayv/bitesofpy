7e9ccb5ff4b69ebf08b82d628860b694e53c400f
nalwayv <nalwayv@googlemail.com>
2019-12-28 23:27:12 +0000

    update

--- bite_238/bite_238.py
+++ bite_238/bite_238.py
@@ -5,10 +5,10 @@ from fibonacci import fib
 import pytest
 
 @pytest.mark.parametrize('fib_num, result', [(2, 1), (4, 3), (10, 55)])
-def test_fib(fib_num, result):
+def test_fib(fib_num, result) -> None:
     assert fib(fib_num) == result
 
 # test error
-def test_fib_ValueError():
+def test_fib_ValueError() -> None:
     with pytest.raises(ValueError):
         assert fib(-1) == 1
