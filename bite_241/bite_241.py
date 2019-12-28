a1b60b914104f74a68c14da8ac2f035006eb4ca6
nalwayv <nalwayv@googlemail.com>
2019-12-28 23:26:13 +0000

    update

--- bite_241/bite_241.py
+++ bite_241/bite_241.py
@@ -13,23 +13,23 @@ from numbers_to_dec import list_to_decimal
 @pytest.mark.parametrize('arr, expected', [([0, 4, 2, 8], 428), 
                                            ([1, 2], 12),
                                            ([3], 3)])
-def test_list_to_decimal(arr, expected):
+def test_list_to_decimal(arr, expected) -> None:
     assert list_to_decimal(arr) == expected
 
 
 @pytest.mark.parametrize('arr', [[6, 2, True], [3.6, 4, 1], ['4', 5, 3, 1]])
-def test_type_error(arr):
+def test_type_error(arr) -> None:
     with pytest.raises(TypeError):
         result = list_to_decimal(arr)
 
 
 @pytest.mark.parametrize('arr', [[-3, 5], [10, 10, 1]])
-def test_value_error(arr):
+def test_value_error(arr) -> None:
     with pytest.raises(ValueError):
         result = list_to_decimal(arr)
 
 
-def test_value_error_out_of_range():
+def test_value_error_out_of_range() -> None:
     with pytest.raises(ValueError):
         value = list_to_decimal([0, 11, 2, 3])
         if value == 1123:
